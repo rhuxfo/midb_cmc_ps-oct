ef58250ea87c6a67230c51f0eab1ce15f4fd19c2
MariPen Yeatts <maripen.yeatts@gmail.com>
2024-08-20 03:32:43 -0500

    Update MStitchFCN_mod.m

--- main_codes/MStitchFCN_mod.m
+++ main_codes/MStitchFCN_mod.m
@@ -55,7 +55,7 @@ for s = 1:length(slicenum)
     %%
     for i = 1:b1
         for j = 1:b2
-            Name = strcat('slice_',num2str(slicenum(s)),'_Tile_',num2str(TileMtrx(i,j)),'_',ch);
+            Name = strcat('slice_',num2str(slicenum(s)),'_tile_',num2str(TileMtrx(i,j)),'_',ch);
             filename = fullfile(Dir,Name);
             sn = strcat('Slice_',num2str(slicenum(s)),'_',ch);
             Sname = strcat(SaveDir,sn);
