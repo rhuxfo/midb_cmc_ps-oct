02085e31455ecaa3e12fa43ca6b23b14925564ec
MariPen Yeatts <maripen.yeatts@gmail.com>
2024-11-19 16:43:15 -0600

    Update check_files_s3_enface.py

--- slurm/check_files_s3_enface.py
+++ slurm/check_files_s3_enface.py
@@ -2,11 +2,11 @@
 import sys
 import os
 # fetch the contents of that directory
-  this_list = os.listdir(f'/tmp/cmc-s3-bucket/')
+this_list = os.listdir(f'/tmp/cmc-s3-bucket/')
 # Check that all files for that slice exist                        
-  for slice_num in range(1, 328):
+for slice_num in range(1, 328):
   filename = f'Slice_{slice_num}_EnR.mat'
 
 # if file not in dir, then print to outfile
   if filename not in this_list:
-  print(filename, flush = True)
+    print(filename, flush = True)
