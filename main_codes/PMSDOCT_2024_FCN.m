82d9585523fb95ba0973d41f130707e96ed1f768
MariPen Yeatts <maripen.yeatts@gmail.com>
2025-01-09 16:30:58 -0600

    Update PMSDOCT_2024_FCN.m
    print file call

--- main_codes/PMSDOCT_2024_FCN.m
+++ main_codes/PMSDOCT_2024_FCN.m
@@ -72,6 +72,7 @@ Nthr = 6.5;
 Flip = P.Flip;
 % Load variables
 filePointer = fopen([data_filename, num2str(slice(1)),P.tileN,num2str(tilenum(1)),'_840_1.dat'], 'r', 'l');
+fprintf('Calling',filePointer);
 headerStr = fgetl(filePointer); %Getting things from the labview
 evalc(headerStr); %Evaluate header from labview to import variables
 % Variables loaded: alineLength; alinePeriod; blineLength; buffersPerFile;
