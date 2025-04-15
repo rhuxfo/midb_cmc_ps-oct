e5049d6dcd2b8cc7a7aa7c2a10535243f2ce2ce9
MariPen Yeatts <maripen.yeatts@gmail.com>
2025-04-15 00:07:25 -0500

    Update EnfaceOffset.m

--- main_codes/EnfaceOffset.m
+++ main_codes/EnfaceOffset.m
@@ -1,56 +1,55 @@
-function Temp2 = EnfaceOffset(En,slice)
+function Temp2 = EnfaceOffset(En,slicenum)
 Temp2 = zeros(8500,11000);
-slicenum = slice;
 
 if slicenum < 20
     Offsetx = 2718;
     Offsety = 3600;
-elseif slicenum > 19 && slicesnum < 33
+elseif slicenum > 19 && slicenum < 33
     Offsetx = 1818;
     Offsety = 2700;
-elseif slicesnum > 32 && slicesnum < 46
+elseif slicenum > 32 && slicenum < 46
     Offsetx = 1818;
     Offsety = 1800;
-elseif slicesnum > 45 && slicesnum < 70
+elseif slicenum > 45 && slicenum < 70
     Offsetx = 918;
     Offsety = 1800;
-elseif slicesnum > 69 && slicesnum < 90
+elseif slicenum > 69 && slicenum < 90
     Offsetx = 918;
     Offsety = 900;
-elseif slicesnum > 89 && slicesnum < 179
+elseif slicenum > 89 && slicenum < 179
     Offsetx = 531;
     Offsety = 900;
-elseif slicesnum > 178 && slicesnum < 202
+elseif slicenum > 178 && slicenum < 202
     Offsetx = 531;
     Offsety = 0;
-elseif slicesnum > 201 && slicesnum < 232
+elseif slicenum > 201 && slicenum < 232
     Offsetx = 0;
     Offsety = 0;
-elseif slicesnum > 231 && slicesnum < 270
+elseif slicenum > 231 && slicenum < 270
     Offsetx = 0;
     Offsety = 900;
-elseif slicesnum >269  && slicesnum < 285
+elseif slicenum >269  && slicenum < 285
     Offsetx = 531;
     Offsety = 900;
-elseif slicesnum > 284 && slicesnum < 307
+elseif slicenum > 284 && slicenum < 307
     Offsetx = 531;
     Offsety = 1800;
-elseif slicesnum > 306 && slicesnum < 313
+elseif slicenum > 306 && slicenum < 313
     Offsetx = 918;
     Offsety = 1800;
-elseif slicesnum > 312 && slicesnum < 326
+elseif slicenum > 312 && slicenum < 326
     Offsetx = 1818;
     Offsety = 2700;
-elseif slicesnum > 325
+elseif slicenum > 325
     Offsetx = 2718;
     Offsety = 2700;
 end
 
-D1 = Offsetx+1;%size(Temp2,1) - (Offsetx + size(TEnR6,1));
+D1 = Offsetx+1;%size(Temp2,1) - (Offsetx + size(En,1));
 D2 = Offsety+1;
 
-% D1 = (size(Temp2,1)-size(TEnR6,1))/2;
-% D2 = (size(Temp2,2)-size(TEnR6,2))/2;
+% D1 = (size(Temp2,1)-size(En,1))/2;
+% D2 = (size(Temp2,2)-size(En,2))/2;
 Temp2(D1:(D1+size(En,1)-1),D2:(D2+size(En,2)-1)) = En;
 
 end
