183cee3845464fc4e5537ad1366885ecdfeb0b66
MariPen Yeatts <maripen.yeatts@gmail.com>
2025-07-29 15:58:10 -0500

    Update CombomaskCrossD.m

--- main_codes/CombomaskCrossD.m
+++ main_codes/CombomaskCrossD.m
@@ -5,11 +5,13 @@ B = size(dB,3);
 LdB = dB(1:endp,:,:);
 Tind = LdB>dBlimit;
 MdB = LdB.*Tind;
-EnOReCal = zeros(A,B)+dBlimit;
+EnOReCal = zeros(A,B);
 for x =1:A
     for y = 1:B
         el = nnz(MdB(:,x,y));
         EnOReCal(x,y) = squeeze(sum(MdB(:,x,y)))/el;
+        if EnOReCal(x,y) == 0
+        EnOReCal(x,y) = dblimit;
     end
 end
 En = EnOReCal;
