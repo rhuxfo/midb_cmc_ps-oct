cfb46e3a6e154e91c8c5f8d9a868e4eb283aa4aa
MariPen Yeatts <maripen.yeatts@gmail.com>
2026-03-03 11:13:34 -0600

    Add files via upload

---
+++ main_codes/Ori2RBG.m
@@ -0,0 +1,22 @@
+function TempOri = Ori2RBG(TEnAO,TEnR)
+
+d1 = size(TEnR,1);
+d2 = size(TEnR,2);
+c = hsv;
+Ang = angle(TEnAO)/2;
+Ang2 = rescale(Ang,1,256);
+Col = zeros(d1,d2,3);
+
+for i = 1:d1
+    for j = 1:d2 
+        ind = round(Ang2(i,j)); 
+        Col(i,j,:) = c(ind,:);
+    end
+end
+
+TR0s = LimdB2D(.95,.2,rescale(TEnR));
+TR0s = rescale(TR0s);
+
+TempOri = Col.*TR0s;
+
+end
