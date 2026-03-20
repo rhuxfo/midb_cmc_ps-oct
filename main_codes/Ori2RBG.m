c064349e46e52bb9befb19970c3c24cb2840a4ec
MariPen Yeatts <maripen.yeatts@gmail.com>
2026-03-20 18:41:44 -0500

    Update Ori2RBG.m

--- main_codes/Ori2RBG.m
+++ main_codes/Ori2RBG.m
@@ -3,18 +3,13 @@ function TempOri = Ori2RBG(TEnAO,TEnR)
 d1 = size(TEnR,1);
 d2 = size(TEnR,2);
 c = hsv(256);
-disp(size(c,1))
 Ang = angle(TEnAO)/2;
 Ang2 = rescale(Ang,1,256);
 Col = zeros(d1,d2,3);
-disp(d1)
-disp(d2)
+
 for i = 1:d1
     for j = 1:d2 
-    disp(i)
-    disp(j)
         ind = round(Ang2(i,j)); 
-        disp(ind)
         Col(i,j,:) = c(ind,:);
     end
 end
