789fa06fc4f1f4cf9a2e0e476ce608e4c0c1dab1
MariPen Yeatts <maripen.yeatts@gmail.com>
2026-04-17 12:09:45 -0500

    Update PMSDOCT_2024_FCN.m
    undo pyrun wrong script

--- main_codes/PMSDOCT_2024_FCN.m
+++ main_codes/PMSDOCT_2024_FCN.m
@@ -316,8 +316,7 @@ for SliceInd=1:length(slice)
                     ON = strcat(save_n,num2str(slice(SliceInd),'%03.f'),'_tile_',num2str(tilenum(TileInd),'%03.f'),'_Cross');
                     SaveFN = fullfile(Save_base,c6,ON);
                     save(SaveFN,'Tile_cross','-v7.3','-nocompression');
-                    pyrunfile("StitchingZarr.py 'SaveFN' 'Cross' 'Tile_cross'")
-                    
+                                        
                 end
                 if calcRetardance == 1
                     ON = strcat(save_n,num2str(slice(SliceInd),'%03.f'),'_tile_',num2str(tilenum(TileInd),'%03.f'),'_R');
